a8b0d1dec5bb72b41a12c97cd5f36f1b79e12215
Ivan Sotelo <isotelo@controlla.com.mx>
2021-10-28 15:27:10 -0600

    [semver:patch] add ls

--- src/scripts/run_composer.sh
+++ src/scripts/run_composer.sh
@@ -1,7 +1,8 @@
-  cp .env.ci.example .env
-  composer install -n --prefer-dist
-  composer run key:generate
-  php artisan vue-i18n:generate
-  php artisan migrate --seed
-  composer run jwt:generate
-  echo "Composer commands is already executed."
+ls
+cp .env.ci.example .env
+composer install -n --prefer-dist
+composer run key:generate
+php artisan vue-i18n:generate
+php artisan migrate --seed
+composer run jwt:generate
+echo "Composer commands is already executed."
