90ed586e9348b066309371680add8b82ba727057
Ivan Sotelo <isotelo@controlla.com.mx>
2021-11-03 12:06:11 -0700

    [semver:patch] update cache options

--- src/scripts/run_composer.sh
+++ src/scripts/run_composer.sh
@@ -1,7 +1,7 @@
 cp .env.ci.example .env
 composer install -n --prefer-dist
 composer run key:generate
-php artisan vue-i18n:generate
 php artisan migrate --seed
+php artisan vue-i18n:generate
 composer run jwt:generate
 echo "Composer commands is already executed."
